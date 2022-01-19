6bb69853bb34702901d00e2f3b9fdc940fa1383c
Joel Chantrell <joelchantrell@gmail.com>
2022-01-19 21:38:46 +1030

    Initial version

---
+++ script.js
@@ -0,0 +1,17 @@
+let myLibrary = [];
+
+function Book() {
+  this.title = title;
+  this.author = author;
+  this.pages = numberOfPages;
+  this.read = readStatus;
+}
+
+function addBookToLibrary() {
+  // do stuff here
+}
+
+
+myLibrary.forEach(book => {
+    myLibrary.push()   
+})
