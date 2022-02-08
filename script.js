c89f54126d67ae42a01110adb2de99045b360add
Joel Chantrell <joelchantrell@gmail.com>
2022-02-08 21:06:28 +1030

    Reformatted + fixed read toggle

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 document.body.addEventListener('click', function(event){
   if(event.target.classList.contains('add')) {
-    addBook(event);
+    addBookForm(event);
   };
   if(event.target.classList.contains('remove')) {
     removeBook(event);
-  };
+  }; 
 });
 
 let Library = [];
@@ -16,65 +16,70 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBook(event) {
+function addBookForm(event) {
   //setup
-  selected = event.target.parentNode;
+  const selected = event.target.parentNode;
   const library = document.querySelector('.library');
+  const submitButton = document.createElement('div');
+  const removeButton = document.createElement('div');
   const title = document.createElement('div');
-  const titleInput = document.createElement('input')
+  const titleInput = document.createElement('input');
   const author = document.createElement('div');
-  const authorInput = document.createElement('input')
+  const authorInput = document.createElement('input');
   const pages = document.createElement('div');
-  const pagesInput = document.createElement('input')
+  const pagesInput = document.createElement('input');
   const read = document.createElement('div');
-  const readInput = document.createElement('input')
-  const submit = document.createElement('button');
-  const buttonInput = document.createElement('button')
-  const bookForm = document.createElement('form');
+  const readText = document.createElement('div');
+  const readSwitch = document.createElement('label');
+  const readCheckbox = document.createElement('input');
+  const readSlider = document.createElement('span');
   const newBook = document.createElement('div');
   const newBookButton = document.createElement('div');
 
+  // clear tile
   selected.classList.remove('new')
   selected.textContent = ''
-  selected.appendChild(titleInput);
-  selected.appendChild(authorInput);
-  selected.appendChild(pagesInput);
-  selected.appendChild(readInput);
-  selected.appendChild(submit);
 
-  // prompt 
+  // populate tile with form info 
+  selected.appendChild(submitButton);
+  submitButton.classList.add('button', 'submit');
+  submitButton.textContent = '✔';
+  selected.appendChild(titleInput);
   titleInput.classList.add('title');
   titleInput.placeholder = 'Title';
+  selected.appendChild(authorInput);
   authorInput.classList.add('author');
   authorInput.placeholder = 'Author';
+  selected.appendChild(pagesInput);
   pagesInput.classList.add('pages');
   pagesInput.placeholder = 'Pages';
-  readInput.classList.add('read');
-  readInput.placeholder = 'Read';
-  submit.classList.add('button', 'submit')
-  submit.textContent = 'Submit';
+  selected.appendChild(read);
+  read.classList.add('read');
+  read.appendChild(readText);
+  readText.classList.add('readText');
+  readText.textContent = 'Read yet?';
+  read.appendChild(readSwitch);
+  readSwitch.classList.add('switch');
+  readSwitch.appendChild(readCheckbox);
+  readCheckbox.type = 'checkbox';
+  readSwitch.appendChild(readSlider);
+  readSlider.classList.add('slider');
 
-
-  //if all fields are valid, create book
-
-  // when submit click
-  // titleInput = title;
-  // etc
+  bookTitle = titleInput.value;
+  console.log(bookTitle)
+  
   let newEntry = new Book(title, author, pages, read);
   Library.push(newEntry);
   console.log(`Added ${newEntry.title} to the library.`);
-
-  //append current new book tile with new book info
-  selected = event.target.parentNode;
   
   //create new book tile
-`  newBook.classList.add('new', 'book');
+  newBook.classList.add('new', 'book');
   newBookButton.classList.add('add');
   library.appendChild(newBook);
   newBook.appendChild(newBookButton);
-  newBookButton.textContent = '+';`
+  newBookButton.textContent = '+';
+  
 }
-
 function removeBook(event){
   selected = event.target.parentNode;
   selected.remove();
